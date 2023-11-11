b3975175cca24cb7cdca1e2db000337e2c596f9f
MiguelRequenaR <e_2019100844l@uncp.edu.pe>
2023-11-11 13:50:43 -0500

    Add wallet, signUp and export

--- app/(tabs)/wallet.tsx
+++ app/(tabs)/wallet.tsx
@@ -1,227 +1,88 @@
-import * as WebBrowser from "expo-web-browser";
-import * as Google from "expo-auth-session/providers/google";
-import * as Facebook from "expo-auth-session/providers/facebook";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-
-import { MaterialIcons } from "@expo/vector-icons";
-import {
-  AppleAuthenticationScope,
-  signInAsync,
-} from "expo-apple-authentication";
-import {
-  Button,
-  FormControl,
-  HStack,
-  Icon,
-  Input,
-  VStack,
-  WarningOutlineIcon,
+import { Pressable, Image, Text, View, SafeAreaView, TextInput} from "react-native";
+import { 
+    Button,
+    FormControl,
+    HStack,
+    Icon,
+    Input,
+    VStack,
+    WarningOutlineIcon,
 } from "native-base";
 import * as React from "react";
-import { Image, Pressable, SafeAreaView, Text, View } from "react-native";
 
-WebBrowser.maybeCompleteAuthSession();
-export default function SignIn() {
+export default function Wallet() {
+  
   const [show, setShow] = React.useState(false);
-  const [userInfo, setUserInfo] = React.useState(null);
-  const [request, response, promptAsync] = Google.useAuthRequest({
-    iosClientId:
-      "422618280931-fc0s3ktar0vcgoc80n128589e5ahhk1e.apps.googleusercontent.com",
-    androidClientId:
-      "422618280931-50inl7uig7t4p5k6o89521jejcic2llj.apps.googleusercontent.com",
-  });
+
   return (
     <SafeAreaView>
-      <View className="flex flex-col space-y-9 justify-between mx-2">
-        <View className="items-center space-y-7">
-          <View className="justify-center text-center  mt-10">
-            <View className="flex flex-row  items-center gap-2 justify-center ">
-              <Image
-                className="w-10 h-10"
-                source={{
-                  uri: "https://img.icons8.com/?size=100&id=ogMD71G6DBkF&format=png",
-                }}
-              />
-              <Text className="font-bold text-xl">Expense Tracker</Text>
+      <View className="flex flex-col space-y-6 justify-between mx-2">
+        <View className="items-center space-y-5">
+            <View>
+                <Text className="text-[#464444] font-bold text-2xl mr-4">
+                    Exportar registros de gastos
+                </Text>
             </View>
-            <Text className="text-textmuted text-center">
-              Controla tus gastos desde tu bolsillo
-            </Text>
-          </View>
-          <VStack space={4}>
-            <FormControl isInvalid={false} w="85%" maxW="350px">
-              <FormControl.Label marginBottom={2}>
-                Tu correo electrónico
-              </FormControl.Label>
-              <Input
-                size="lg"
-                borderRadius={7}
-                w={{
-                  base: "90%",
-                  md: "25%",
-                }}
-                placeholder="joanhweu115@gmail.com"
-              />
-              <FormControl.ErrorMessage
-                leftIcon={<WarningOutlineIcon size="xs" />}
-              >
-                Verifica este campo
-              </FormControl.ErrorMessage>
-            </FormControl>
-            <FormControl isInvalid={true} w="85%" maxW="350px">
-              <FormControl.Label marginBottom={2}>Contraseña</FormControl.Label>
-              <Input
-                size="lg"
-                w={{
-                  base: "90%",
-                  md: "25%",
-                }}
-                type={show ? "text" : "password"}
-                passwordRules={
-                  "required: upper; required: lower; required: digit; minlength: 8;"
-                }
-                borderRadius={7}
-                InputRightElement={
-                  <Pressable onPress={() => setShow(!show)}>
-                    <Icon
-                      as={
-                        <MaterialIcons
-                          name={show ? "visibility" : "visibility-off"}
-                        />
-                      }
-                      size={5}
-                      mr="2"
-                      color="muted.400"
-                    />
-                  </Pressable>
-                }
-                placeholder="********"
-              />
-              <FormControl.ErrorMessage
-                leftIcon={<WarningOutlineIcon size="xs" />}
-              >
-                Contraseña incorrecta
-              </FormControl.ErrorMessage>
-            </FormControl>
-          </VStack>
-
-          <Button
-            colorScheme="teal"
-            className="rounded-full"
-            height={12}
-            w={{
-              base: "83%",
-              md: "25%",
-            }}
-            maxW="350px"
-          >
-            <Text className="font-semibold text-white ">Ingresar</Text>
-          </Button>
         </View>
-        <View className="flex flex-row items-center text-center justify-center">
-          <View className="w-36 border-[1px] h-0.5 border-gray-300"></View>
-          <Text className="text-textmuted mx-2 text-center">o</Text>
-          <View className="w-36 border-[1px] h-0.5 border-gray-300"></View>
-        </View>
-        <View className="flex gap-3 items-center  justify-center">
-          <Button
-            variant="outline"
-            colorScheme="gray"
-            className="rounded-full  "
-            height={12}
-            w={{
-              base: "80%",
-              md: "25%",
-            }}
-            maxW="350px"
-          >
-            <Pressable onPress={() => promptAsync()}>
-              <HStack alignItems="center" space={2}>
-                <Image
-                  className="w-5 h-5"
-                  source={{
-                    uri: "https://img.icons8.com/?size=96&id=17949&format=png",
-                  }}
-                />
-                <Text className="  font-semibold">
-                  Iniciar Sesión con Google
+          <View className="bg-[#ffff] mx-auto w-[85%] rounded-lg">
+            <HStack>
+              <View className="bg-[#CABDFF] w-[3%] rounded-full my-3 ml-3"><Text></Text></View>
+                <Text className="text-[#464444] p-3 font-bold text-lg">
+                    Exportar como...
                 </Text>
-              </HStack>
-            </Pressable>
-          </Button>
-          <Button
-            height={12}
-            variant="outline"
-            colorScheme="gray"
-            className="rounded-full  "
-            w={{
-              base: "80%",
-              md: "25%",
-            }}
-            maxW="350px"
-          >
-            <Pressable
-              onPress={async () => {
-                try {
-                  const credential = await signInAsync({
-                    requestedScopes: [
-                      AppleAuthenticationScope.FULL_NAME,
-                      AppleAuthenticationScope.EMAIL,
-                    ],
-                  });
-                  // signed in
-                } catch (e) {
-                  if (e.code === "ERR_REQUEST_CANCELED") {
-                    // handle that the user canceled the sign-in flow
-                  } else {
-                    // handle other errors
-                  }
-                }
-              }}
-            >
-              <HStack alignItems="center" space={2}>
-                <Image
-                  className="w-5 h-5"
-                  source={{
-                    uri: "https://img.icons8.com/?size=60&id=95294&format=png",
-                  }}
-                />
-                <Text className="  font-semibold">
-                  Iniciar Sesión con Apple
-                </Text>
-              </HStack>
-            </Pressable>
-          </Button>
-          <Button
-            height={12}
-            variant="outline"
-            colorScheme="gray"
-            className="rounded-full  "
-            w={{
-              base: "80%",
-              md: "25%",
-            }}
-            maxW="350px"
-          >
-            <HStack alignItems="center" space={2}>
-              <Image
-                className="w-5 h-5"
-                source={{
-                  uri: "https://img.icons8.com/?size=96&id=uLWV5A9vXIPu&format=png",
-                }}
-              />
-              <Text className="  font-semibold">Iniciar Sesión con Meta</Text>
             </HStack>
-          </Button>
-        </View>
-        <HStack alignItems="center" justifyContent="center">
-          <Text className="text-textmuted text-center">
-            ¿No tienes una cuenta?
-          </Text>
-          <Button className="px-1" variant="link" colorScheme="teal">
-            Regístrate
-          </Button>
-        </HStack>
+            <View className="w-72 mx-auto border-b border-[#AEACAC] mt-3"></View>
+            <VStack>
+                <View className="mt-4 ml-3">
+                  <Text className=" text-[#464444]">
+                      Selecciona el tipo de documento en el que desea exportar
+                  </Text>
+                </View>
+                <View className="flex flex-row items-center p-4 border m-4 border-[#368983] rounded-xl">
+                  <Image
+                      className="w-7 h-7 mr-2"
+                      source={{
+                        uri: "https://img.icons8.com/?size=48&id=13013&format=png",
+                      }}
+                  />
+                  <Input
+                    size="sm"
+                    borderRadius={7}
+                    w={{
+                      base: "90%",
+                      md: "25%",
+                    }}
+                    placeholder="Ingresa el monto de meta de ahorro"
+                />
+                </View>   
+                <View className="flex flex-row items-center p-4 border m-4 border-[#368983] rounded-xl">
+                  <Image
+                      className="w-7 h-7 mr-2"
+                      source={{
+                        uri: "https://img.icons8.com/?size=80&id=TVHK3ohcpSmp&format=gif",
+                      }}
+                  />
+                  <Input
+                    size="sm"
+                    borderRadius={7}
+                    w={{
+                      base: "90%",
+                      md: "25%",
+                    }}
+                    placeholder="Ingresa la fecha limite de ahorro"
+                />
+                </View>   
+                <Button
+                    colorScheme="teal"
+                    className="rounded-full mb-4 ml-48"
+                    height={10}
+                    w={40}
+                    maxW="100px"
+                  >
+                    <Text className="font-semibold text-white ">Registrar</Text>
+                </Button>  
+            </VStack>
+          </View>        
       </View>
     </SafeAreaView>
   );
