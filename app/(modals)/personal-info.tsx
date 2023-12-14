4ea708aede908745ed8188324732a7755465ac9e
Joan <joan300501@gmail.com>
2023-12-13 21:23:20 -0800

    Update personal-info.tsx and package.json
    This commit updates the personal-info.tsx file by removing unused imports, rearranging imports, and adding a new function for picking images. It also updates the package.json file by adding dependencies for expo-document-picker and expo-image-picker.

--- app/(modals)/personal-info.tsx
+++ app/(modals)/personal-info.tsx
@@ -1,23 +1,23 @@
-import { FontAwesome } from "@expo/vector-icons";
 import { supabase } from "@/utils/supabase";
-import { useForm, Controller } from "react-hook-form";
 import {
   Button,
-  HStack,
-  VStack,
-  Input,
-  Box,
   FormControl,
+  HStack,
+  Input,
+  VStack,
   WarningOutlineIcon,
-  Divider,
 } from "native-base";
 import * as React from "react";
-import { Image, Pressable, Text, View } from "react-native";
-import DocumentPicker from 'react-native-document-picker';
+import { Controller, useForm } from "react-hook-form";
+import { Image, Text, View } from "react-native";
+import DocumentPicker from "react-native-document-picker";
+import * as ImagePicker from "expo-image-picker";
 
 export default function PersonalInfo() {
   const [show, setShow] = React.useState(false);
-  const [imageUri, setImageUri] = React.useState("https://userstock.io/data/wp-content/uploads/2017/09/bewakoof-com-official-219589-300x300.jpg");
+  const [imageUri, setImageUri] = React.useState(
+    "https://userstock.io/data/wp-content/uploads/2017/09/bewakoof-com-official-219589-300x300.jpg"
+  );
   const {
     control,
     handleSubmit,
@@ -26,47 +26,47 @@ export default function PersonalInfo() {
   } = useForm();
 
   async function onSubmit(data: any) {
-    try{
+    try {
       const { error } = await supabase
-        .from('usuarios_expense')
+        .from("usuarios_expense")
         .update({
           nombres: data.name,
           apellidos: data.lastname,
         })
-        .eq('id', data.id)
-      if(error) {
+        .eq("id", data.id);
+      if (error) {
         throw new Error(error.message);
       }
-      alert('Datos actualizados correctamente')
-    }
-    catch(e){
+      alert("Datos actualizados correctamente");
+    } catch (e) {
       alert(e.message);
     }
   }
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
 
-  async function handleReplaceClick() {
-      try {
-        const res = await DocumentPicker.pickSingle({
-          type: [DocumentPicker.types.images],
-        });
+    //todo Here should be placed the new image uri
+    setImageUri(result.assets[0].uri);
 
-        console.log(
-          res.uri,
-          res.type,
-          res.name,
-          res.size
-        );
-        setImageUri(res.uri);
-        setValue('image', res.uri);
-      } catch (err) {
-        if (DocumentPicker.isCancel(err)) {
-          // El usuario canceló la operación de selección de archivos
-        } else {
-          throw err;
-        }
-      }
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert("You did not select any image.");
     }
+  };
 
+  React.useEffect(() => {
+    (async () => {
+      const { granted } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (!granted) {
+        alert("Permission was not granted");
+      }
+    })();
+  }, []);
   return (
     <View className="flex flex-col space-y-3 justify-between mx-auto">
       <HStack>
@@ -83,7 +83,7 @@ export default function PersonalInfo() {
       >
         <Image
           source={{
-            uri: imageUri
+            uri: imageUri,
           }}
           alt="profile-pic"
           width={70}
@@ -96,7 +96,7 @@ export default function PersonalInfo() {
             colorScheme="gray"
             height={10}
             px={4}
-            onPress={handleReplaceClick}
+            onPress={pickImageAsync}
           >
             <Text className="text-white font-semibold">Reemplazar</Text>
           </Button>
@@ -105,7 +105,7 @@ export default function PersonalInfo() {
           </Button>
         </HStack>
       </HStack>
-      
+
       <View className="border-[1px] my-5 h-0.5 border-gray-300"></View>
       <VStack space={5}>
         <FormControl isInvalid={!!errors.name}>
